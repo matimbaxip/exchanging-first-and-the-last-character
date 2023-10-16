b00627d840533eacc352deacacf58af997494db5
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-16 14:35:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exchanging_first_and_the_last_character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+
+            string result = ExchangeFirstAndLastCharacters(input);
+
+            Console.WriteLine("Result: " + result);
+
+        }
+        static string ExchangeFirstAndLastCharacters(string input)
+        {
+            if (input.Length <= 1)
+                return input;
+
+            char firstChar = input[0];
+            char lastChar = input[input.Length - 1];
+
+            return lastChar + input.Substring(1, input.Length - 2) + firstChar;
+        }
+
+    }
+}
